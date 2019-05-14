d8894f87b0261d30b1638b4ab93aa325594b87fc
Charlie <charliekelly31415@gmail.com>
2019-05-14 08:44:56 -0400

    Add search button and event handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 import './SearchBar.css'
 
-function SearchBar () {
-  return (
-    <form id="searchForm" className="search-bar">
-      <input type="text" className="search-text" id="searchField" placeholder="Search for courses, tutors" />
-    </form>
-  )
+const searchHandler = (event) => {
+  event.preventDefault()
+  // perform search
 }
+
+const SearchBar = () => (
+  <form id="searchForm" className="search-bar">
+    <input type="text" className="search-text" id="searchField" placeholder="Search for courses, tutors" />
+    <a className="btn" href="#search" onClick={searchHandler}>&nbsp;</a>
+  </form>
+)
 
 export default SearchBar
