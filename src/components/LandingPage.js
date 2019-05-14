214e79739bffb6e364a8f0eb8a34166e64823375
Charlie <charliekelly31415@gmail.com>
2019-05-13 22:21:57 -0400

    render as function

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,14 +3,18 @@ import React from 'react'
 import './LandingPage.css'
 import SearchBar from './SearchBar'
 
-const LandingPage = (
-  <div className="hero">
-    <div className="main-title">
-      <div>Learn and Teach,</div>
-      <div>Whenever Wherever</div>
+function LandingPage () {
+  return (
+    <div className="splash">
+      <div className="overlay">
+        <div className="main-title">
+          <div>Learn and Teach,</div>
+          <div>Whenever Wherever</div>
+        </div>
+        <SearchBar />
+      </div>
     </div>
-    <SearchBar />
-  </div>
-)
+  )
+}
 
 export default LandingPage
