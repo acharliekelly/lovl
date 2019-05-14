f105cded23d1af5b11cb5741199b40f32b6af13b
Charlie <charliekelly31415@gmail.com>
2019-05-13 22:20:20 -0400

    remove Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './css/index.scss'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
 import { HashRouter } from 'react-router-dom'
 
