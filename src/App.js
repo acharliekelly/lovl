2734e1583b643089d3eb7e182c712b777d4e28b8
Charlie <charliekelly31415@gmail.com>
2019-05-13 22:22:52 -0400

    render as function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
 
-import Header from './components/Header'
+import NavBar from './components/NavBar'
 import LandingPage from './components/LandingPage'
 import SignUpPage from './components/SignUpPage'
 import LoginPage from './components/LoginPage'
 
-const App = () => (
-  <Container>
-    <Header />
-    <Route exact path="/" component={LandingPage} />
-    <Route path="/signup" component={SignUpPage} />
-    <Route path="/login" component={LoginPage} />
-  </Container>
-)
+function App () {
+  return (
+    <div className="wrapper">
+      <NavBar />
+      <Route exact path="/" component={LandingPage} />
+      <Route path="/signup" component={SignUpPage} />
+      <Route path="/login" component={LoginPage} />
+    </div>
+  )
+}
 
 export default App
