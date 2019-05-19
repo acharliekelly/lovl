49391a65e7f681b1fef6192da59354e839ee7a77
Charlie <charliekelly31415@gmail.com>
2019-05-19 19:13:17 -0400

    fix ;

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-import NavBar from './components/NavBar'
-import LandingPage from './components/LandingPage'
-import SignUpPage from './components/SignUpPage'
-import LoginPage from './components/LoginPage'
+import NavBar from './components/NavBar';
+import LandingPage from './components/LandingPage';
+import SignUpPage from './components/SignUpPage';
+import LoginPage from './components/LoginPage';
 
 function App () {
   return (
@@ -14,7 +14,7 @@ function App () {
       <Route path="/signup" component={SignUpPage} />
       <Route path="/login" component={LoginPage} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
